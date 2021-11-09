ceafe29514868edf7e7f84d6bab2c0b6070e2cb0
ATRAPA\jerryqwang <jerryqwang@deloitte.com.cn>
2021-11-09 10:02:18 +0800

    流程报表+弹窗管理静态

---
+++ src/views/popupWindowManagement/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <iPage >
+    <pageHeader class="title">{{language('弹窗管理')}}</pageHeader>
+    <div class="content">
+        <theTableFilter />
+        <theTableList />
+    </div>
+      
+  </iPage>
+</template>
+
+<script>
+import {iPage} from 'rise'
+import pageHeader from '@/components/pageHeader'
+import {theTableFilter,theTableList} from './components'
+export default {
+    components:{iPage,theTableFilter,theTableList,pageHeader}
+}
+</script>
+
+<style lang="scss" scoped>
+.content{
+  margin-top:20px ;
+}
+</style>
