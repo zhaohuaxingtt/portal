8d68838007151dd07b6634cb398a12185b284967
kuangshp <kuangshp@126.com>
2021-11-17 16:39:43 +0800

    feat: 左右切换

--- src/views/assistant/problemMan/components/problemHandler.vue
+++ src/views/assistant/problemMan/components/problemHandler.vue
@@ -1,13 +1,54 @@
 <template>
-  <div></div>
+  <div style="width: 100%;">
+    <div class="nav">
+      <el-tabs v-model="currentName">
+        <el-tab-pane label="供应商用户" name="supplierUser">
+          <ProblemHandlerPanel type="1" v-if="currentName==='supplierUser'"/>
+        </el-tab-pane>
+        <el-tab-pane label="内部用户" name="internalUser">
+          <ProblemHandlerPanel type="2" v-if="currentName==='internalUser'"/>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
+import ProblemHandlerPanel from './problemHandlerPanel';
 export default {
-
+  data() {
+    return {
+      currentName: 'supplierUser',
+    }
+  },
+  components: {
+    ProblemHandlerPanel,
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.nav {
+  ::v-deep .el-tabs__active-bar {
+    background-color:transparent!important;
+  }
+  ::v-deep .el-tabs__nav-wrap::after {
+    background-color:transparent!important;
+  }
+  ::v-deep .el-tabs__item {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+  ::v-deep .el-tabs__item.is-active {
+    background-color:#fff !important;
+    height:35px;
+    padding-left: 20px;
+    padding-right: 20px;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.08);
+    border-radius:3px;
+  }
+}
+
 
 </style>
---
+++ src/views/assistant/problemMan/components/problemHandlerPanel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex flex-row content">
+    <div class="left-content">左侧</div>
+    <div class="right-content ml20">右侧</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: Number,
+      default: 1,
+    }
+  },
+  mounted() {
+    console.log(this.type,'???');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../comon.scss';
+.content {
+  width: 100%;
+  height: 94%;
+  .left-content{
+    width: 28%;
+    min-height: 100%;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 10px rgba(27, 29, 33, 0.08);
+    opacity: 1;
+    border-radius: 5px;
+    padding: 15px 20px 0px 20px;
+    overflow-y: auto;
+  }
+  .right-content{
+    width: 100%;
+    height: 100%;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 10px rgba(27, 29, 33, 0.08);
+    opacity: 1;
+    border-radius: 5px;
+    padding: 30px 40px 20px 40px;
+  }
+}
+</style>
