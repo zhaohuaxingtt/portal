12d6aaa6ff7537520b3029a2f00a07e2ccd317b8
18996399290 <18996399290@163.com>
2021-11-18 10:17:19 +0800

    卡片饼状图修改

--- src/views/home/components/Sponser/index.vue
+++ src/views/home/components/Sponser/index.vue
@@ -215,19 +215,19 @@ export default {
           item.name =
             item.name +
             '    ' +
-            `${(item.num / totalSum).toFixed(2) * 100}.00%` +
+            `${String((item.num / totalSum)) == 'NaN' ? '0' : (item.num / totalSum).toFixed(2) * 100 }.00%` +
             '   '
         } else if (item.name.length == 2) {
           item.name =
             item.name +
             '      ' +
-            `${(item.num / totalSum).toFixed(2) * 100}.00%` +
+            `${String((item.num / totalSum)) == 'NaN' ? '0' : (item.num / totalSum).toFixed(2) * 100 }.00%` +
             '   '
         } else {
           item.name =
             item.name +
             '        ' +
-            `${(item.num / totalSum).toFixed(2) * 100}.00%` +
+            `${String((item.num / totalSum)) == 'NaN' ? '0' : (item.num / totalSum).toFixed(2) * 100 }.00%` +
             '    '
         }
       })
@@ -250,7 +250,7 @@ export default {
             let name = data.data.name.split( /\s+/)[0]
             return `${name}:<br/>
             ${total}家<br/>
-            ${(data.data.num / total).toFixed(2) * 100}.00%
+            ${String((data.data.num / total)) === 'NaN' ? '0' : (data.data.num / total).toFixed(2) * 100}.00%
             `
           }
         },
@@ -373,7 +373,7 @@ export default {
             }
             const type = data[0].data.type
             return `${type}-Rating数量：${total}<br/>${type}-Rating比例：${
-              (total / totalCount).toFixed(2) * 100
+              String((total / totalCount)) === 'NaN' ? '0.00' : (total / totalCount).toFixed(2) * 100
             }%`
           }
         },
