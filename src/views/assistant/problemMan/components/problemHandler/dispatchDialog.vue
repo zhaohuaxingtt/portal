753f3b377dc1dd96455845655c5c91de436c4df3
kuangshp <kuangshp@126.com>
2021-11-18 11:13:31 +0800

    feat: 指派弹框

--- src/views/assistant/problemMan/components/problemHandler/dispatchDialog.vue
+++ src/views/assistant/problemMan/components/problemHandler/dispatchDialog.vue
@@ -1,30 +1,23 @@
 <template>
-  <iDialog title="转派" style="margin-top:20vh" :visible.sync="show" v-if="show" width="22%" @close='closeDiologBtn' append-to-body>
-    <div>内容区域</div>
+  <iDialog title="转派" style="margin-top:20vh" :visible.sync="show" v-if="show" width="30%" @close='closeDiologBtn' append-to-body>
+    <div class="flex flex-row justify-between">
+      <div style="width:80px;">管理员</div>
+      <iSelect v-model="query.type" filterable placeholder="请输入">
+        <el-option v-for="item in options" :key="item.code" :label="item.value" :value="item.code"></el-option>
+      </iSelect>
+    </div>
     <div class="reset_style">
-      <iButton @click="close">{{language('确认')}}</iButton>
-      <iButton @click="save">{{language('取消')}}</iButton>
+      <iButton @click="save">{{language('确认')}}</iButton>
+      <iButton @click="close">{{language('取消')}}</iButton>
     </div>
   </iDialog>
 </template>
 
 <script>
-import { iDialog, iButton } from 'rise'
+import { iDialog, iButton,iSelect } from 'rise'
 export default {
-  name: 'notifyDialog',
-  components: { iDialog, iButton },
+  name: 'dispatchDialog',
   props: {
-    detail: {
-      type: Object,
-      default: function () {
-        return {
-          title: '',
-          content: '',
-          picUrl: '',
-          linkUrl: ''
-        }
-      }
-    },
     show: {
       type: Boolean,
       default: false
@@ -32,7 +25,11 @@ export default {
   },
   data () {
     return {
-      visible: false
+      visible: false,
+      query: {
+        type:'',
+      },
+      options: [],
     }
   },
   methods: {
@@ -41,17 +38,21 @@ export default {
     },
     close () {
       this.closeDiologBtn();
-      this.callback('cancel')
     },
     save () {
       this.closeDiologBtn();
-      this.callback('confirm')
     }
+  },
+  components: {
+    iDialog, 
+    iButton,
+    iSelect,
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../../../comon.scss';
 .reset_style {
   margin-top: 50px;
   padding-bottom: 20px;
