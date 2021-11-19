22d1dd59c4baf63c0901dfa0c245df2719e214a9
ATRAPA\jerryqwang <jerryqwang@deloitte.com.cn>
2021-11-19 14:32:06 +0800

    新建关键词

---
+++ src/views/assistant/problemMan/components/keyWordsManagement/data.js
@@ -0,0 +1,19 @@
+
+
+export const   TABLE_KEYWORDS_COLUMNS=[
+    {
+        type:'selection'
+    },
+    {
+        type:'index',
+        label:'序号'
+    },
+    {
+        label:'关键词',
+        prop:'keyWords'
+    },
+    {
+        label:"添加时间",
+        prop:'addTime'
+    }
+]
