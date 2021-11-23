97d1e9f892c95bb6a904b402d9e3ace6df9d25d4
18996399290 <18996399290@163.com>
2021-11-23 18:08:34 +0800

    去掉左下角图标及添加悬浮事件

--- src/components/iLayout/components/leftLayout.vue
+++ src/components/iLayout/components/leftLayout.vue
@@ -33,7 +33,7 @@
           </span>
         </div>
         <div class="btn-button">
-          <img src="~@/assets/images/leftContent.png" alt="" />
+          <!-- <img src="~@/assets/images/leftContent.png" alt="" /> -->
         </div>
       </div>
     </div>
@@ -309,11 +309,11 @@ export default {
   .btn-button {
     width: 70px;
     height: 70px;
-    background: #f1f5ff;
+    // background: #f1f5ff;
     margin: 0 auto;
     padding: 13px;
-    border-radius: 15px;
-    cursor: pointer;
+    // border-radius: 15px;
+    // cursor: pointer;
 
     img {
       display: inline-block;
