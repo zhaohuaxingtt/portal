6da611baa00545e2eaa911c738fa1971499836bf
曹鹏 <1076613617@qq.com>
2021-11-29 13:52:46 +0800

    opcs路由开发，修复其他bug

--- src/views/opcsSupervise/commonHeardNav/navData.js
+++ src/views/opcsSupervise/commonHeardNav/navData.js
@@ -8,4 +8,20 @@ export const tabRouterList = [
     },
    
   ]
-  
+  export const applicationRouterList = [
+    {
+      value: 1,
+      name: '应用管理',
+      url: '/opcs/application/manage',
+      activePath: '/opcs/application/manage',
+      key: 'YINGYONGGUANLI'
+    },
+    // {
+    //     value: 2,
+    //     name: '应用管理',
+    //     url: '/opcs/application/manage',
+    //     activePath: '/opcs/application/manage',
+    //     key: 'YINGYONGGUANLI'
+    //   },
+   
+  ]
---
+++ src/views/opcsSupervise/opcsPermission/application/manage.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Date: 2021-11-29 10:20:21
+ * @LastEditors: caopeng
+ * @LastEditTime: 2021-11-29 11:25:41
+ * @FilePath: \front-portal-new\src\views\opcsSupervise\opcsPermission\application\manage.vue
+-->
+<template>
+  <div>
+      111
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
