a8ec5298a72158312eda0c9acbb53e3afc990638
qyu <dewoer@outlook.com>
2021-12-01 12:00:06 +0800

    冲突处理

--- vue.config.js
+++ vue.config.js
@@ -124,19 +124,7 @@ module.exports = {
   //引入全局css变量
   css: {
     //是否开起css分离
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     extract: false,
-=======
-    extract: true,
->>>>>>> bd4735047... iDrawer 点击空白处关闭
-=======
-    extract: true,
->>>>>>> bd4735047... iDrawer 点击空白处关闭
-=======
-    extract: true,
->>>>>>> bd4735047... iDrawer 点击空白处关闭
     sourceMap: process.env.NODE_ENV === 'production',
     requireModuleExtension: true,
     loaderOptions: {
