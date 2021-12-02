f003ca50b21c9b8ba26a9712271c3d943f86e153
曹鹏 <1076613617@qq.com>
2021-12-02 18:10:09 +0800

    fixbug

--- src/views/supplierManagement/supplier360/list/joinBlackApply/index.vue
+++ src/views/supplierManagement/supplier360/list/joinBlackApply/index.vue
@@ -108,8 +108,9 @@ export default {
     }
   },
   created() {
-    // var tbody =window.document.getElementById('appRouterView');
-    // window.parent.postMessage({ key: 'setFormHeight', value: tbody.offsetHeight+'px'},'*')
+          var tbody = document.body
+        var height = tbody.clientHeight
+        window.parent.postMessage({ key: 'setFormHeight', value: height+'px'},'*')
     if (window.top === window.self) {
       this.ifSelf = true
     } else {
