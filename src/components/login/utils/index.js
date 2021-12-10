d31c2e9e24f59f71ac6fd808f407ae04bd9ff765
qyu <ext-yuqiang@csvw.com>
2021-12-10 11:34:24 +0800

    网络封装、login组件

--- src/components/login/utils/index.js
+++ src/components/login/utils/index.js
@@ -1,4 +1,4 @@
-import JSEncrypt from 'jsencrypt'
+import JSEncrypt from './jsencrypt.min.js'
 const publicKey = `MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDtHWBZlZFuWCQmAqWGH5NaYimrmWHI8/BgHcorxAlq+5bCJyVe8VD9Zm4CRksP/yYPAqWB03B0wm1F1ePObs3ddhpk/MJ8GzbXJY0inj2J0n90QEzhebb/GpHt777EmfY/PuA4fGw8WtW6LHwkr+tnluKlR9f5ZgKByf3mBEge0wIDAQAB`
 
 /**
@@ -19,4 +19,9 @@ export function encryptRsa(str) {
  */
 export function encryptPwd(str) {
   return encryptRsa(str)
+}
+
+export function setToken(tokenData) {
+  // eslint-disable-next-line no-undef
+  return Cookies.set(process.env.VUE_APP_TOKEN_NAME, tokenData)
 }
