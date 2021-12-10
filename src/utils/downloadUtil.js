324bc8ea5e9f023f169abb531dfe06e19959050f
zhonganjing <1qaz!QAZ>
2021-12-10 18:11:18 +0800

    update the meeting download method

--- src/utils/downloadUtil.js
+++ src/utils/downloadUtil.js
@@ -23,42 +23,7 @@ const download = ({
   type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
   noFileUd = false
 }) => {
-  if (noFileUd) {
-    request({
-      url: url ? url : MOCK_FILE_URL + '?isDown=true&fileId=' + fileIds,
-      method: 'get',
-      responseType: 'blob', // 响应类型必须设置
-      getResponse: true
-    })
-      .then((response) => {
-        let blob = new Blob([response], { type })
-        console.log(blob)
-        const blobUrl = window.URL.createObjectURL(blob)
-        createAnchorLink(blobUrl, filename)
-        window.URL.revokeObjectURL(blobUrl)
-        callback && callback(true)
-      })
-      .catch(() => {
-        callback && callback(false)
-      })
-  } else {
-    requestFile({
-      url: url ? url : MOCK_FILE_URL + '?isDown=true&fileId=' + fileIds,
-      // method: 'post',
-      method: 'get',
-      responseType: 'blob', // 响应类型必须设置
-      getResponse: true
-    })
-      .then((response) => {
-        const blobUrl = window.URL.createObjectURL(response)
-        createAnchorLink(blobUrl, filename)
-        window.URL.revokeObjectURL(blobUrl)
-        callback && callback(true)
-      })
-      .catch(() => {
-        callback && callback(false)
-      })
-  }
+    createAnchorLink(MOCK_FILE_URL + '?isDown=true&fileId=' + fileIds, filename);
 }
 
 export { download, createAnchorLink }
