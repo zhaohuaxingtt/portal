bd7e5680d42a3a25d17178ebb607921a5a7213ce
胡帅 <hushuai>
2021-12-15 14:42:15 +0800

    会议列表copy  待修改

--- src/views/meeting/managementHall/meetingList/index.vue
+++ src/views/meeting/managementHall/meetingList/index.vue
@@ -3,159 +3,177 @@
  * @FilePath: \front-portal\front-portal\src\views\meeting\managementHall\meetingList\index.vue
 -->
 <template>
-  <div>
-    <!-- 查询区 -->
-    <div class="SearchOptions">
-      <i-search @sure="sure" @reset="reset">
-        <template #button>
-          <div>
-            <iButton @click="sure">{{ $t('rfq.SEARCH') }}</iButton>
-            <iButton @click="reset">{{ $t('rfq.RFQRESET') }}</iButton>
-          </div>
-        </template>
-        <el-form>
-            <el-row >
-                <el-col :span="12">
-                  <!-- 会议名称  cscCode-->
-          <el-form-item :label="$t('MT_HUIYIMINGCHENG')">
-            <i-input
-              :placeholder="$t('LK_QINGSHURU')"
-              class=""
-            >
-              <!-- v-model="formData.cscCode" -->
-            </i-input>
-          </el-form-item>
-                </el-col>
-                <el-col :span="6">
-                  <!-- 会议类型  cscCode-->
-          <el-form-item :label="$t('MT_HUIYILEIXING')">
-            <i-input
-              :placeholder="$t('LK_QINGXUANZE')"
-              class=""
-            >
-              <!-- v-model="formData.cscCode" -->
-            </i-input>
-          </el-form-item>
-                </el-col>
-                <el-col :span="6">
-                  <!-- 状态  cscCode-->
-          <el-form-item :label="language('状态', '状态')">
-            <i-input
-              :placeholder="$t('LK_QINGXUANZE')"
-              class=""
-            >
-              <!-- v-model="formData.cscCode" -->
-            </i-input>
-          </el-form-item>
-                </el-col>
-            </el-row>
-
-
-            <el-row>
-              <el-col :span="12">
-                <!-- 会议日期   startDate  endDate-->
-          <el-form-item
-            :label="language('会议日期', '会议日期')"
-            class="formwidth"
-          >
-              <!-- v-model="biddingStartDate" -->
-            <el-date-picker
-              class="date-picker-width"
-              type="datetimerange"
-              range-separator="至"
-              start-placeholder="开始日期"
-              end-placeholder="结束日期"
-              value-format="yyyy-MM-dd HH:mm:ss"
-              @change="changebiddingDate"
-            >
-            </el-date-picker>
-          </el-form-item>
-
-              </el-col>
-              <el-col :span="6">
-                 <!-- 周次  cscCode-->
-          <el-form-item :label="language('周次', '周次')">
-            <i-input
-              :placeholder="$t('LK_QINGXUANZE')"
-              class=""
-            >
-              <!-- v-model="formData.cscCode" -->
-            </i-input>
-          </el-form-item>
-              </el-col>
-            </el-row>
-
-            
-
-         
-          
-          
-          
-        </el-form>
-      </i-search>
-    </div>
-    <!-- 列表 -->
-    <iCard class="margin-top20">
-      <div class="OrganizationTable">
-        <iTableCustom
-          style="width: 100%"
-          :loading="loading"
-          :data="tableData"
-          :columns="tableColumns"
-          :customClass="'custom-class'"
-          @go-detail="goDetail"
-          @go-quoteMethod="goquoteMethod"
-          :tree-expand="tableExpanded"
-          @click-menu="handleExpend"
-           @go-quoteMethodDetail="goquoteMethodDetail"
-        />
-        <iPagination
-          v-update
-          @size-change="handleSizeChange($event, getList)"
-          @current-change="handleCurrentChange($event, getList)"
-          background
-          :current-page="page.currPage"
-          :page-sizes="page.pageSizes"
-          :page-size="page.pageSize"
-          :layout="page.layout"
-          :total="page.totalCount"
-        />
-      </div>
-    </iCard>
+  <div class="meeting-home">
+    <theSearch
+      @searchTableList="searchTableList"
+      @handleSearchReset="handleSearchReset"
+      @setTypeObj="setTypeObj"
+      @deleteWeek="deleteWeek"
+      :form="form"
+    />
+    <theTable
+      v-loading="loading"
+      ref="theTable"
+      :page="page"
+      :total="page.total"
+      :tableListData="tableListData"
+      :tableListColumns="tableListColumns"
+      :typeObject="typeObject"
+      :attendeeList="attendeeList"
+      :receiverList="receiverList"
+      :meetingTypeList="meetingTypeList"
+      @handleChangePage="handleChangePage"
+      @handleSizePage="handleSizePage"
+      @getTableList="getTableList"
+      :meetingTypeName="form.meetingType ? form.meetingType.name : ''"
+    />
   </div>
 </template>
 
 <script>
+import theSearch from './components/theSearch.vue'
+import theTable from './components/theTable.vue'
+import { getMettingList } from '@/api/meeting/home'
+import { getAttendee, getReceiver } from '@/api/meeting/type'
 import { pageMixins } from '@/utils/pageMixins'
-import iTableCustom from '@/components/iTableCustom'
-import { iSearch, iInput, iSelect, iCard, iButton, iPagination } from 'rise'
-export default {
-  mixins: [pageMixins],
-  components: {
-    iSearch,
-    iInput,
-    iSelect,
-    iCard,
-    iButton,
-    iPagination,
-    iTableCustom,
-  },
+import resultMessageMixin from '@/mixins/resultMessageMixin'
+import { tableListColumns } from './components/data'
+// import {deleteString} from '@/utils/utils.js';
 
+export default {
+  mixins: [pageMixins, resultMessageMixin],
+  components: {
+    theSearch,
+    theTable
+  },
+  props: {
+    //页面需要的menuType参数
+    menuType: {
+      type: String,
+      default: 'admin' //原来的路由大厅是admin，tab页上的cf
+    }
+  },
+  data() {
+    return {
+      tableListData: [],
+      tableListColumns,
+      typeObject: {},
+      form: {
+        states: [],
+        name: '',
+        // meetingTypeId: "",
+        meetingType: {},
+        startDate: '',
+        startTime: '',
+        meetingPlace: '',
+        receiverId: '',
+        attendeeId: '',
+        attendee: '',
+        weekOfYears: []
+      },
+      meetingTypeList: [],
+      attendeeList: [],
+      receiverList: [],
+      loading: false
+    }
+  },
+  mounted() {
+    this.getTableList()
+    this.getSelectListAll()
+  },
+  // watch: {
+  //   form: {
+  //     handler(v) {
+  //       console.log("form", v);
+  //     },
+  //     immediate: true,
+  //     deep: true,
+  //   },
+  // },
+  methods: {
+    getTableList() {
+      let param = {
+        ...this.form,
+        meetingTypeId: this.form.meetingType ? this.form.meetingType.id : '',
+        pageNum: this.page.currPage,
+        pageSize: 10,
+        states: this.form.states ? [this.form.states] : []
+      }
+      Reflect.deleteProperty(param, 'meetingType')
+      this.query(param)
+    },
+    searchTableList() {
+      this.page.currPage = 1
+      this.getTableList()
+    },
+    query(e) {
+      this.loading = true
+      getMettingList(e)
+        .then((res) => {
+          this.tableListData = res.data
+          this.page.total = res.total
+        })
+        .finally(() => (this.loading = false))
+    },
+    handleSearchReset() {
+      this.form = {
+        weekOfYears: [],
+        startDate: '',
+        startTime: ''
+      }
+      this.page.currPage = 1
+      this.getTableList()
+    },
+    handleChangePage(e) {
+      this.page.currPage = e
+      let param = {
+        pageNum: this.page.currPage,
+        pageSize: this.page.pageSize,
+        ...this.form,
+        meetingTypeId: this.form.meetingType ? this.form.meetingType.id : '',
+        states: this.form.states ? [this.form.states] : []
+      }
+      this.query(param)
+    },
+    handleSizePage(e) {
+      this.page.pageSize = e
+      let param = {
+        pageNum: this.page.currPage,
+        pageSize: e,
+        ...this.form,
+        meetingTypeId: this.form.meetingType ? this.form.meetingType.id : '',
+        states: this.form.states ? [this.form.states] : []
+      }
+      this.query(param)
+    },
+    setTypeObj(e) {
+      let typeObj = {}
+      this.meetingTypeList = e
+      e.forEach((item) => {
+        typeObj[item.id] = item.name
+      })
+      this.typeObject = typeObj
+    },
+    getSelectListAll() {
+      const param = {
+        pageNum: 1,
+        pageSize: 999
+      }
+      getAttendee(param).then((res) => {
+        this.attendeeList = res.data
+      })
+      getReceiver(param).then((res) => {
+        this.receiverList = res.data
+      })
+    },
+    deleteWeek() {
+      this.form.weekOfYears = []
+    }
+  }
 }
 </script>
-
 <style lang="scss" scoped>
-::v-deep .date-picker-width {
-  width: 30.5rem !important;
-  // margin-bottom: 2.125rem !important;
-  float: left !important;
-  margin-right: 3.125rem !important;
-  padding-left: 0.125rem !important;
-  padding-top: 0.3125rem !important;
-  padding-bottom: 0.3125rem !important;
-  height: 2.1875rem !important;
+.meeting-home {
+  width: 1740px;
 }
-::v-deep .formwidth {
-  width: 30.6rem !important;
-}
-</style> 
+</style>
