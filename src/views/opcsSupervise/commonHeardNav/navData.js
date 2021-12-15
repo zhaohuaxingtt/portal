5fe62a8d39fb81d142d0ae66280df7f607376037
曹鹏 <1076613617@qq.com>
2021-12-15 18:07:12 +0800

    opcs联调

--- src/views/opcsSupervise/commonHeardNav/navData.js
+++ src/views/opcsSupervise/commonHeardNav/navData.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-11-25 09:47:22
  * @LastEditors: caopeng
- * @LastEditTime: 2021-12-08 18:16:04
+ * @LastEditTime: 2021-12-15 17:31:17
  * @FilePath: \front-portal-new\src\views\opcsSupervise\commonHeardNav\navData.js
  */
 export const tabRouterList = [
@@ -18,16 +18,16 @@ export const tabRouterList = [
     {
       value: 1,
       name: '应用管理',
-      url: '/opcs/list/application/manage',
-      activePath: '/opcs/list/application/manage',
+      url: '/provider/opcs/list/application/manage',
+      activePath: '/provider/opcs/list/application/manage',
       key: '应用管理',
       icon:'el-icon-s-grid'
     },
     {
         value: 2,
         name: '联系人与用户管理',
-        url: '/opcs/list/application/userManage',
-        activePath: '/opcs/list/application/userManage',
+        url: '/provider/opcs/list/application/userManage',
+        activePath: '/provider/opcs/list/application/userManage',
         key: '联系人与用户管理',
         icon:'el-icon-s-check'
       },
