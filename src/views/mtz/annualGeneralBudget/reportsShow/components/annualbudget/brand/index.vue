8c9a5898197599291cf5821e0998fde3cbcf74e7
简华阳 <jhy673369178@163.com>
2021-12-24 14:27:15 +0800

    增加仅看自己  和年份选择组件

--- src/views/mtz/annualGeneralBudget/reportsShow/components/annualbudget/brand/index.vue
+++ src/views/mtz/annualGeneralBudget/reportsShow/components/annualbudget/brand/index.vue
@@ -1,13 +1,52 @@
 <template>
-<div>品牌</div>
+  <i-card>
+     <div class='title-box'>
+       <div class='left-box'>
+         <span>2021年总金额:340,400,200人民币</span>
+         <span class='span-1'>2020年总⾦额：240,400,200⼈⺠币</span>
+       </div>
+       <div  class='right-box'>
+         <show-me-components/>
+
+         <year-change-components/>
+       </div>
+
+
+     </div>
+  </i-card>
 </template>
 
 <script>
+import {iCard,iSelect} from 'rise'
+import ShowMeComponents from '@/views/mtz/annualGeneralBudget/reportsShow/components/comm/ShowMeComponents'
+import YearChangeComponents from '@/views/mtz/annualGeneralBudget/reportsShow/components/comm/YearChangeComponents'
 export default {
-  name: 'index'
+  name: 'index',
+  components:{
+    YearChangeComponents,
+    ShowMeComponents,
+    iCard,
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang='scss'>
+.title-box{
+  display: flex;
+  justify-content: space-between;
+  .left-box{
+    span{
+      font-size: 20px;
+      font-weight: bold;
+      color: black;
+    }
+    .span-1{
+      margin-left: 20px;
+    }
+  }
+}
+.right-box{
+  display: flex;
+  justify-content: flex-start;
+}
 </style>
---
+++ src/views/mtz/annualGeneralBudget/reportsShow/components/comm/YearChangeComponents.vue
@@ -0,0 +1,33 @@
+<template>
+ <div class='year-change'>
+   <span>{{  language('LK_QIEHUANNIANFEN', '切换年份') }}</span>
+   <i-select class='my_select'></i-select>
+ </div>
+</template>
+
+<script>
+import {iSelect} from 'rise'
+export default {
+  name: 'YearChangeComponents',
+  components:{
+    iSelect,
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+.year-change{
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  span {
+    font-size: 20px;
+    color: #000000;
+    align-content: center;
+  }
+  .my_select{
+    margin-left: 10px;
+    width:auto !important;
+  }
+}
+</style>
