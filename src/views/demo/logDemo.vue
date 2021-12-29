b4ad3c9a963b995145ff9e0eb263465a5eb94e7b
邓涛涛 <dengtaotao@gzzy.com>
2021-12-29 10:29:47 +0800

    log 测试

--- src/views/demo/logDemo.vue
+++ src/views/demo/logDemo.vue
@@ -3,13 +3,13 @@
     <el-button @click="open">open</el-button>
     <iLog :show.sync="showDialog" :bizId="bizId" is-page />
     <el-button @click="showDialog1 = true">open user log</el-button>
-    <iUserLog :show.sync="showDialog1" bizId="" menuId="2" is-page />
+    <iUserLog :show.sync="showDialog1" bizId="1" menuId="2" is-page />
   </iPage>
 </template>
 
 <script>
-import { iPage, iLog } from 'rise'
-import iUserLog from '@/components/iUserLog'
+import { iPage, iLog,iUserLog } from 'rise'
+// import iUserLog from '@/components/iUserLog'
 export default {
   data() {
     return {
