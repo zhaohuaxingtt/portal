4673d3975a44599cb570865648c3a40cd61b46ab
ArthurLyu <artlyu@deloitte.com.cn>
2022-01-04 14:19:20 +0800

    CRW-1856

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,23 @@ import routeConfig from './Router.js'
 // eslint-disable-next-line no-undef
 Vue.use(VueRouter)
 
+
 // eslint-disable-next-line no-undef
 const router = new VueRouter({
-  base: process.env.VUE_APP_PUBLICPATH,
-  mode: 'hash',
-  scrollBehavior() {
-    return { x: 0, y: 0 }
-  },
-  routes: routeConfig.routes
+    base: process.env.VUE_APP_PUBLICPATH,
+    mode: 'hash',
+    scrollBehavior() {
+        return { x: 0, y: 0 }
+    },
+    routes: routeConfig.routes
 })
 
 router.afterEach(() => {
-  // Remove initial loading
-  const appLoading = document.getElementById('app-loading')
-  if (appLoading) {
-    appLoading.style.display = 'none'
-  }
+    // Remove initial loading
+    const appLoading = document.getElementById('app-loading')
+    if (appLoading) {
+        appLoading.style.display = 'none'
+    }
 })
 
 export default router
