0065936383ce060fc1d74d4998deaa19ee98b72c
dengtaotao <18785262993@163.com>
2022-01-06 11:27:45 +0800

    内容管理

--- src/views/reportForm/admin/content/editContent.vue
+++ src/views/reportForm/admin/content/editContent.vue
@@ -1,36 +1,36 @@
 <template>
    <iDialog
-      title="添加内容"
+      :title="form.id ? '修改内容' : '添加内容'"
       :visible.sync="show"
       width="500px"
       @close="handleClose"
       @open="open"
     >
         <el-form :model="form" ref="form" label-width="90px" :inline="false" size="normal">
-            <el-form-item :label="language('报表类型')" prop="type" :rules="{ required:true, message:'请选择', trigger:'change'}">
+            <iFormItem :label="language('报表类型')" prop="type" :rules="{ required:true, message:'请选择', trigger:'change'}">
                 <iSelect v-model="form.type" filterable clearable>
                     <el-option v-for="item in status" :key="item.id" :label="item.lableName" :value="item.id"></el-option>
                 </iSelect>
-            </el-form-item>
-            <el-form-item :label="language('报表标题')" prop="name" :rules="{ required:true, message:'请输入', trigger:'blur'}">
+            </iFormItem>
+            <iFormItem :label="language('报表标题')" prop="name" :rules="{ required:true, message:'请输入', trigger:'blur'}">
                 <iInput v-model="form.name"></iInput>
-            </el-form-item>
-            <el-form-item :label="language('发布日期')" prop="date" :rules="{ required:true, message:'请输入', trigger:'blur'}">
+            </iFormItem>
+            <iFormItem :label="language('发布日期')" prop="date" :rules="{ required:true, message:'请输入', trigger:'blur'}">
                 <iDatePicker
                     value-format="yyyy-MM-dd"
                     type="date"
                     v-model="form.date"
                     style="width:100%"
                     />
-            </el-form-item>
-            <el-form-item :label="language('报表分类')" prop="cate" :rules="{ required:true, message:'请选择', trigger:'change'}">
+            </iFormItem>
+            <iFormItem :label="language('报表分类')" prop="cate" :rules="{ required:true, message:'请选择', trigger:'change'}">
                 <iSelect v-model="form.cate" filterable clearable>
                     <el-option v-for="item in status" :key="item.id" :label="item.lableName" :value="item.id"></el-option>
                 </iSelect>
-            </el-form-item>
-            <el-form-item :label="language('上传附件')" prop="file" :rules="{ required:true, message:'请选择', trigger:'change'}">
+            </iFormItem>
+            <iFormItem :label="language('上传附件')" prop="file" :rules="{ required:true, message:'请选择', trigger:'change'}">
                 <iUpload v-model="form.file" btnTxt="选择文件"></iUpload>
-            </el-form-item>
+            </iFormItem>
         </el-form>
         
         <span slot="footer" class="dialog-footer">
