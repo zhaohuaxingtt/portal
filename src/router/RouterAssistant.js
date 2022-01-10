7698ce95a1cf0c70ef4562f1ee2af4a6aceb9bda
qyu <ext-yuqiang@csvw.com>
2022-01-10 15:59:14 +0800

    问答菜单

--- src/router/RouterAssistant.js
+++ src/router/RouterAssistant.js
@@ -1,26 +1,26 @@
 export default [
-	{
-		path: '/assistant/problemMan',
-		name: 'problemMan',
-		meta: {
-			title: 'problemMan'
-		},
-		component: () => import('@/views/assistant/problemMan')
-	},
   {
-		path: '/assistant/helpCenter',
-		name: 'helpCenter',
-		meta: {
-			title: 'helpCenter'
-		},
-		component: () => import('@/views/assistant/helpCenter')
-	},
-	{
-		path: '/assistant/helpCenterMan',
-		name: 'helpCenterMan',
-		meta: {
-			title: 'helpCenterMan',
-		},
-		component: () => import('@/views/assistant/helpCenterMan')
-	},
+    path: '/assistant/problemMan',
+    name: 'problemMan',
+    meta: {
+      title: 'problemMan'
+    },
+    component: () => import('@/views/assistant/problemMan')
+  },
+  {
+    path: '/assistant/helpCenter',
+    name: 'helpCenter',
+    meta: {
+      title: 'helpCenter'
+    },
+    component: () => import('@/views/assistant/helpCenter')
+  },
+  {
+    path: '/assistant/helpCenterMan',
+    name: 'helpCenterMan',
+    meta: {
+      title: 'helpCenterMan'
+    },
+    component: () => import('@/views/assistant/helpCenterMan')
+  }
 ]
--- src/views/assistant/problemMan/index.vue
+++ src/views/assistant/problemMan/index.vue
@@ -1,9 +1,12 @@
 <template>
   <iPage>
     <!-- <div class="flex justify-between"> -->
-      <pageHeader class="title">
-        {{language('问题管理')}}
-         <div class="types" slot="actions">
+    <pageHeader class="title">
+      <!-- {{ language('问题管理') }} -->
+
+      <iNavMvp :list="menusLevel3" :lev="1" lang router-page />
+
+      <div class="types" slot="actions">
         <iTabBadge>
           <iTabBadgeItem
             :active="helpMoudle === 'problemHandler'"
@@ -33,9 +36,9 @@
           />
         </iTabBadge>
       </div>
-        </pageHeader>
-      <!-- <div class="content-title">{{ language('问题管理') }}</div> -->
-     
+    </pageHeader>
+    <!-- <div class="content-title">{{ language('问题管理') }}</div> -->
+
     <!-- </div> -->
     <div
       class="flex flex-row content mt20"
@@ -55,7 +58,7 @@
     >
       <ModuleManagement />
     </div>
-		<div
+    <div
       class="flex flex-row content mt20"
       v-if="helpMoudle === 'labelManagement'"
     >
@@ -71,14 +74,14 @@
 </template>
 
 <script>
-import { iPage } from 'rise'
+import { iPage, iNavMvp } from 'rise'
 import { iTabBadge, iTabBadgeItem } from '@/components/iTabBadge'
-import ProblemHandler from './components/problemHandler/index';
+import ProblemHandler from './components/problemHandler/index'
 import LabelManagement from './components/labelManagement/index'
 import ModuleManagement from './components/moduleManagement/index'
 import ProblemStatement from './components/problemStatement/index'
 import KeyWordsManagement from './components/keyWordsManagement'
-import { questionUnReplyCountApi} from '@/api/assistant';
+import { questionUnReplyCountApi } from '@/api/assistant'
 import pageHeader from '@/components/pageHeader'
 import store from '@/store'
 export default {
@@ -87,30 +90,108 @@ export default {
       text: '问答处理',
       helpMoudle: 'problemHandler',
       problemHandlerCount: 0,
+      menusLevel3: []
+    }
+  },
+  computed: {
+    fullMenus() {
+      return this.$store.state.permission.menuList
     }
   },
   methods: {
-		tabChange(val) {
-			this.$router.replace({path:this.$route.path,query:{module:val}})
-			this.helpMoudle = val
-		},
+    tabChange(val) {
+      this.$router.replace({ path: this.$route.path, query: { module: val } })
+      this.helpMoudle = val
+    },
     // 获取未读问题数量
     async questionUnReplyCount(selfOnly) {
-      const response = await questionUnReplyCountApi(selfOnly);
+      const response = await questionUnReplyCountApi(selfOnly)
       if (response?.code === '200') {
-        this.problemHandlerCount = response.data;
+        this.problemHandlerCount = response.data
       }
     },
     changeSelfHandle(val) {
-      this.questionUnReplyCount(val);
+      this.questionUnReplyCount(val)
+    },
+    // 3级菜单显示
+    getMenusLevel3() {
+      const menus = this.getSiblingMenus(
+        this.fullMenus,
+        '/assistant/problemMan',
+        '',
+        ''
+      )
+
+      const menusLevel3 = this.generateMenuData(menus, 3)
+      this.menusLevel3 = menusLevel3
+      console.log('getMenusLevel3', menusLevel3)
+    },
+    getSiblingMenus(data, path, menuType, activePath) {
+      for (let i = 0; i < data.length; i++) {
+        const element = data[i]
+        const url =
+          element.url && element.url.indexOf('#') > -1
+            ? element.url.split('#')[1]
+            : element.url
+        if (
+          `${path}?menuType=${menuType}` === url ||
+          url === path ||
+          url === activePath
+        ) {
+          return data
+        }
+        if (element.menuList) {
+          const res = this.getSiblingMenus(
+            element.menuList,
+            path,
+            menuType,
+            activePath
+          )
+          if (res) {
+            return res
+          }
+        }
+      }
+    },
+    generateMenuData(diyMenus, level) {
+      diyMenus = diyMenus || []
+      const res = []
+      diyMenus.forEach((e, index) => {
+        let purePath = e.url || ''
+        if (e.url && e.url.includes('#')) {
+          purePath = e.url.split('#')[1]
+        }
+        const purePathArr = purePath.split('/')
+        let activePath =
+          level === 3
+            ? '/' + purePathArr[1]
+            : '/' + purePathArr[1] + '/' + purePathArr[2]
+        if (activePath.indexOf('?')) {
+          activePath = activePath.split('?')[0]
+        }
+        const url =
+          e.url && e.url.indexOf('#') > -1 ? e.url.split('#')[1] : e.url
+        res.push({
+          value: index + 1,
+          name: e.name,
+          message: 0,
+          url: url,
+          activePath: activePath,
+          key: e.name,
+          permissionKey: e.permissionKey,
+          permissionName: e.name
+        })
+      })
+      return res
     }
-	},
-  created(){
-		this.helpMoudle = this.$route.query.module || "problemHandler"
+  },
+  created() {
+    this.helpMoudle = this.$route.query.module || 'problemHandler'
+    this.getMenusLevel3()
   },
   mounted() {
-    console.log(store.state, 'store.state');
-    this.questionUnReplyCount(1);
+    console.log(store.state, 'store.state')
+    this.questionUnReplyCount(1)
   },
   components: {
     iPage,
@@ -121,14 +202,15 @@ export default {
     ModuleManagement,
     ProblemStatement,
     KeyWordsManagement,
-    pageHeader
+    pageHeader,
+    iNavMvp
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../comon.scss';
-.content{
+.content {
   height: 100%;
 }
 .content-title {
