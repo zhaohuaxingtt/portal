5091078c6f2ae9ee3feb2318a671d4e9cdf42169
曹鹏 <1076613617@qq.com>
2022-01-13 18:16:34 +0800

    opcs需求更改与新需求开发

--- src/views/opcsSupervise/opcsPermission/application/manage/index.vue
+++ src/views/opcsSupervise/opcsPermission/application/manage/index.vue
@@ -1,13 +1,13 @@
 <!--
  * @Date: 2021-11-29 10:20:21
  * @LastEditors: caopeng
- * @LastEditTime: 2021-11-29 14:47:27
+ * @LastEditTime: 2022-01-13 10:35:15
  * @FilePath: \front-portal-new\src\views\opcsSupervise\opcsPermission\application\manage\index.vue
 -->
 <template>
   <div>
-    <baseInfo style="margin-top:20px"></baseInfo>
-    <manageTable style="margin-top:20px"></manageTable>
+    <!-- <baseInfo style="margin-top:20px"></baseInfo> -->
+    <manageTable style="margin-top:10px"></manageTable>
   </div>
 </template>
 
