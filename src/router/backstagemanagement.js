7a7217c3ae1183d1b27ad170e2809584d2d277ba
wentliao <wentliao@deloitte.com.cn>
2022-01-17 15:20:28 +0800

    迁移评分设置代码至portal

---
+++ src/router/backstagemanagement.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: wentliao
+ * @Date: 2021-12-30 17:36:03
+ * @Description: 评分设置-- 暂时放在souring里面 后期会移动到protal里面
+ */
+
+export default [
+    {
+      path: "/backstagemanagement",
+      meta: { title: "设置评分管理"},
+      component: () => import("@/views/backstagemanagement"),
+      redirect: "/backstagemanagement/configscoredept",
+      children: [
+        {
+            path: "/backstagemanagement/configscoredept",
+            name: "configscoredept",
+            meta: { title: "评分部门设置"},
+            component: () => import("@/views/backstagemanagement/configscoredept"),
+        },
+        {
+            path: "qualityscorerules",
+            name: "qualityscorerules",
+            meta: { title: "质量评分预设规则"},
+            component: () => import("@/views/backstagemanagement/qualityscorerules"),
+          }
+      ]
+    }
+  ]
