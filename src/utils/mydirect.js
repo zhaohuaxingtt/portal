f53a98180d42cc312d9d7133a5067eaf826bf6e5
zhaohuaxing <vinzhao@deloitte.com.cn>
2022-01-24 16:34:29 +0800

    申请bdl

--- src/utils/mydirect.js
+++ src/utils/mydirect.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yuszhou
  * @Date: 2021-02-19 14:29:09
- * @LastEditTime: 2022-01-12 19:24:32
+ * @LastEditTime: 2022-01-24 11:18:59
  * @LastEditors: Please set LastEditors
  * @Description: 自定义指令文件。
  * @FilePath: \front-portal-new\src\utils\mydirect.js
@@ -43,6 +43,7 @@ Vue.directive('permission', {
       ) {
         // 处理控件中，不可见的组件 列入：Ibutton.
         //-----------------------2022-1-20 修改权限配置内容start------------------------------
+        console.log(process.env.NODE_ENV, '===========')
         if (['VMSIT', 'SIT'].includes(process.env.NODE_ENV)) {
           if (!store.state.permission.whiteBtnList[pagePermission]) {
             el.parentNode.removeChild(el)
