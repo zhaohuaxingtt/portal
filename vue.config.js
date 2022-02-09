1467819cdfc4cb653da29bde133d5c929bfb4457
qyu <ext-yuqiang@csvw.com>
2022-02-09 15:13:58 +0800

    CRW-3304

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,8 @@ const postcss = px2rem({
   remUnit: 16
 })
 
-const BASE_IP = '10.122.17.38'
-// const BASE_IP = '10.122.18.166'
+// const BASE_IP = '10.122.17.38'
+const BASE_IP = '10.122.18.166'
 
 module.exports = {
   publicPath: process.env.VUE_APP_PUBLICPATH,
@@ -243,8 +243,8 @@ module.exports = {
         }
       },
       [process.env.VUE_APP_MTZ]: {
-        // target: `http://${BASE_IP}:8046/mtz`,
-        target: `http://rise-nginx-internal.apps.vmocp-test.csvw.com/mtzApi`,
+        target: `http://${BASE_IP}:8046/mtz`,
+        // target: `http://rise-nginx-internal.apps.vmocp-test.csvw.com/mtzApi`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_MTZ]: ''
