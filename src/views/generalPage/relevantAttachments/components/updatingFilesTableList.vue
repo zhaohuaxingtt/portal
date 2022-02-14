4eeccee1943c09ec36190d6b0906c40058c83b5f
zhaohuaxing <vinzhao@deloitte.com.cn>
2022-02-14 19:46:35 +0800

    下载文件

--- src/views/generalPage/relevantAttachments/components/updatingFilesTableList.vue
+++ src/views/generalPage/relevantAttachments/components/updatingFilesTableList.vue
@@ -1,137 +1,188 @@
 <!--
  * @Author: moxuan
  * @Date: 2021-04-13 17:30:36
- * @LastEditTime: 2022-01-25 17:04:51
- * @LastEditors: YoHo
+ * @LastEditTime: 2022-02-14 19:44:06
+ * @LastEditors: Please set LastEditors
  * @Description: 附件上传表格
 -->
 <template>
-  <el-table :height="height" tooltip-effect='light' :data='tableData' :empty-text="$t('LK_ZANWUSHUJU')"
-            v-loading='tableLoading' @selection-change="handleSelectionChange">
-    <el-table-column v-if="selection" type='selection' width="50" align='center'></el-table-column>
-    <el-table-column v-if='index' type='index' width='50' align='center' label='#'></el-table-column>
+  <el-table :height="height"
+            tooltip-effect='light'
+            :data='tableData'
+            :empty-text="$t('LK_ZANWUSHUJU')"
+            v-loading='tableLoading'
+            @selection-change="handleSelectionChange">
+    <el-table-column v-if="selection"
+                     type='selection'
+                     width="50"
+                     align='center'></el-table-column>
+    <el-table-column v-if='index'
+                     type='index'
+                     width='50'
+                     align='center'
+                     label='#'></el-table-column>
     <template v-for="(items,index) in tableTitle">
       <!-- 附件名称-->
-      <el-table-column :key="index" align='center' :width="items.width" :show-overflow-tooltip='items.tooltip'
-                       v-if="items.props === 'templateName'" :prop="items.props"
-                       :label="items.key ? $t(items.key) : items.name" :fixed="items.fixed">
+      <el-table-column :key="index"
+                       align='center'
+                       :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       v-if="items.props === 'templateName'"
+                       :prop="items.props"
+                       :label="items.key ? $t(items.key) : items.name"
+                       :fixed="items.fixed">
         <template slot-scope="scope">
           <span :class="{
-            'openLinkText': scope.row.fileName && !scope.row.isCommitment,
-            'underline': scope.row.fileName && !scope.row.isCommitment}"
-                @click="handleFileDownload(scope.row)"
-          >
+            'openLinkText': scope.row.fileId && !scope.row.isCommitment,
+            'underline': scope.row.fileId && !scope.row.isCommitment}"
+                @click="handleFileDownload(scope.row)">
             {{ scope.row.templateName }}</span>
-          <span class="required  margin-left5" v-if="scope.row.required">*</span>
-          <el-popover
-              trigger="hover"
-              :content="scope.row.remark"
-              placement="top-start">
-            <icon slot="reference" symbol name="iconxinxitishi" class="exampleFileIconStyle margin-left5"
-                  v-if="scope.row.remark"/>
+          <span class="required  margin-left5"
+                v-if="scope.row.required">*</span>
+          <el-popover trigger="hover"
+                      :content="scope.row.remark"
+                      placement="top-start">
+            <icon slot="reference"
+                  symbol
+                  name="iconxinxitishi"
+                  class="exampleFileIconStyle margin-left5"
+                  v-if="scope.row.remark" />
           </el-popover>
         </template>
       </el-table-column>
       <!-- 操作-->
-      <el-table-column :key="index" align='center' :width="items.width" :show-overflow-tooltip='items.tooltip'
-                       v-if="items.props === 'isCommitment'" :prop="items.props"
-                       :label="items.key ? $t(items.key) : items.name" :fixed="items.fixed">
+      <el-table-column :key="index"
+                       align='center'
+                       :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       v-if="items.props === 'isCommitment'"
+                       :prop="items.props"
+                       :label="items.key ? $t(items.key) : items.name"
+                       :fixed="items.fixed">
         <template slot-scope="scope">
           <template v-if="scope.row.templateId=='21'">
-            <iButton type="text" class="openLinkText text-500" :disabled="disabled" @click="publish(scope.row)">确认并发布</iButton>
-            <iButton type="text" class="openLinkText text-500" @click="viewPublish(scope.row)">查看已发布</iButton>
+            <iButton type="text"
+                     class="openLinkText text-500"
+                     :disabled="disabled"
+                     @click="publish(scope.row)">确认并发布</iButton>
+            <iButton type="text"
+                     class="openLinkText text-500"
+                     @click="viewPublish(scope.row)">查看已发布</iButton>
           </template>
-          <span v-else-if="scope.row.isCommitment" class="openLinkText cursor"
+          <span v-else-if="scope.row.isCommitment"
+                class="openLinkText cursor"
                 @click="handleViewAttachment(scope.row)">{{ $t('LK_CHAKAN') }}</span>
-          <uploadButton
-              :showText="true"
-              @uploadedCallback="handleUploadedCallback($event,scope.row)"
-              button-text="LK_SHANGCHUAN"
-              v-else/>
+          <uploadButton :showText="true"
+                        @uploadedCallback="handleUploadedCallback($event,scope.row)"
+                        button-text="LK_SHANGCHUAN"
+                        v-else />
         </template>
       </el-table-column>
       <!-- 上传/确认日期-->
-      <el-table-column :key="index" align='center' :width="items.width" :show-overflow-tooltip='items.tooltip'
-                       v-if="items.props === 'createDateTime'" :prop="items.props"
-                       :label="items.key ? $t(items.key) : items.name" :fixed="items.fixed">
+      <el-table-column :key="index"
+                       align='center'
+                       :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       v-if="items.props === 'createDateTime'"
+                       :prop="items.props"
+                       :label="items.key ? $t(items.key) : items.name"
+                       :fixed="items.fixed">
         <template slot-scope="scope">
           <span>{{ scope.row.createDateTime }}</span>
         </template>
       </el-table-column>
 
       <!-- 备注说明-->
-      <el-table-column :width="items.width" :show-overflow-tooltip='items.tooltip' :key="index" align='center'
-                       v-else-if="items.props === 'attachRemark'" :prop="items.props"
+      <el-table-column :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       :key="index"
+                       align='center'
+                       v-else-if="items.props === 'attachRemark'"
+                       :prop="items.props"
                        :label="items.key ? $t(items.key) : items.name">
         <template slot-scope="scope">
-          <i-input v-model="scope.row.attachRemark" v-if="scope.row.attachId && !scope.row.isCommitment" :maxlength="300"/>
+          <i-input v-model="scope.row.attachRemark"
+                   v-if="scope.row.attachId && !scope.row.isCommitment"
+                   :maxlength="300" />
           <span v-else>{{ scope.row.attachRemark }}</span>
         </template>
       </el-table-column>
 
       <!--样例文件-->
-      <el-table-column :width="items.width" :show-overflow-tooltip='items.tooltip' :key="index" align='center'
-                       v-else-if="items.props === 'templateDemoUrl'" :prop="items.props"
+      <el-table-column :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       :key="index"
+                       align='center'
+                       v-else-if="items.props === 'templateDemoUrl'"
+                       :prop="items.props"
                        :label="items.key ? $t(items.key) : items.name">
         <template #header>
           {{ items.key ? $t(items.key) : items.name }}
-          <el-popover
-              trigger="hover"
-              width="220"
-              :content="$t('SUPPLIER_EXAMPLEDSEC')"
-              placement="top-start">
-            <icon slot="reference" symbol name="iconxinxitishi" class="exampleFileIconStyle"/>
+          <el-popover trigger="hover"
+                      width="220"
+                      :content="$t('SUPPLIER_EXAMPLEDSEC')"
+                      placement="top-start">
+            <icon slot="reference"
+                  symbol
+                  name="iconxinxitishi"
+                  class="exampleFileIconStyle" />
           </el-popover>
         </template>
         <template slot-scope="scope">
-          <span class="openLinkText cursor" @click="handleExampleDownload(scope.row)"
-                v-if="scope.row.templateDemoUrl && !scope.row.isCommitment"
-          >{{ $t('LK_XIAZAI') }}</span>
+          <span class="openLinkText cursor"
+                @click="handleExampleDownload(scope.row)"
+                v-if="scope.row.templateDemoUrl && !scope.row.isCommitment">{{ $t('LK_XIAZAI') }}</span>
         </template>
       </el-table-column>
 
       <!--不能提供原因-->
-      <el-table-column :width="items.width" :show-overflow-tooltip='items.tooltip' :key="index" align='center'
-                       v-else-if="items.props === 'notAvailableReason'" :prop="items.props"
+      <el-table-column :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       :key="index"
+                       align='center'
+                       v-else-if="items.props === 'notAvailableReason'"
+                       :prop="items.props"
                        :label="items.key ? $t(items.key) : items.name">
         <template slot-scope="scope">
-          <i-input
-              v-model="scope.row.notAvailableReason"
-              v-if="scope.row.isDisplayReason"
-              :maxlength="300"
-          />
+          <i-input v-model="scope.row.notAvailableReason"
+                   v-if="scope.row.isDisplayReason"
+                   :maxlength="300" />
         </template>
       </el-table-column>
 
       <!--最晚提供时间-->
-      <el-table-column :width="items.width" :show-overflow-tooltip='items.tooltip' :key="index" align='center'
-                       v-else-if="items.props === 'lastAvailableTime'" :prop="items.props"
+      <el-table-column :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       :key="index"
+                       align='center'
+                       v-else-if="items.props === 'lastAvailableTime'"
+                       :prop="items.props"
                        :label="items.key ? $t(items.key) : items.name">
         <template slot-scope="scope">
-          <iDatePicker
-              value-format="yyyy-MM-dd"
-              v-model="scope.row.lastAvailableTime"
-              v-if="scope.row.isDisplayLastTime"
-              type="date"
-              :placeholder="$t('SUPPLIER_XUANZERQI')"
-              :picker-options="{disabledDate(time) {return time.getTime() < Date.now() - 24 * 60 * 60 * 1000}}"
-          />
+          <iDatePicker value-format="yyyy-MM-dd"
+                       v-model="scope.row.lastAvailableTime"
+                       v-if="scope.row.isDisplayLastTime"
+                       type="date"
+                       :placeholder="$t('SUPPLIER_XUANZERQI')"
+                       :picker-options="{disabledDate(time) {return time.getTime() < Date.now() - 24 * 60 * 60 * 1000}}" />
         </template>
       </el-table-column>
 
       <!--证件有效期-->
-      <el-table-column :width="items.width" :show-overflow-tooltip='items.tooltip' :key="index" align='center'
-                       v-else-if="items.props === 'validityOfCertificate'" :prop="items.props"
+      <el-table-column :width="items.width"
+                       :show-overflow-tooltip='items.tooltip'
+                       :key="index"
+                       align='center'
+                       v-else-if="items.props === 'validityOfCertificate'"
+                       :prop="items.props"
                        :label="items.key ? $t(items.key) : items.name">
         <template slot-scope="scope">
-          <iDatePicker
-              value-format="yyyy-MM-dd"
-              v-model="scope.row.validityOfCertificate"
-              v-if="scope.row.isDisplayValidity"
-              type="date"
-              :placeholder="$t('SUPPLIER_XUANZERQI')"
-              :picker-options="{disabledDate(time) {return time.getTime() < Date.now() - 24 * 60 * 60 * 1000}}"
-          />
+          <iDatePicker value-format="yyyy-MM-dd"
+                       v-model="scope.row.validityOfCertificate"
+                       v-if="scope.row.isDisplayValidity"
+                       type="date"
+                       :placeholder="$t('SUPPLIER_XUANZERQI')"
+                       :picker-options="{disabledDate(time) {return time.getTime() < Date.now() - 24 * 60 * 60 * 1000}}" />
         </template>
       </el-table-column>
 
@@ -139,18 +190,18 @@
   </el-table>
 </template>
 <script>
-import {iInput, icon, iDatePicker, iButton} from 'rise'
+import { iInput, icon, iDatePicker, iButton } from 'rise'
 import uploadButton from '../../../../../src/components/uploadButton'
 
 export default {
   props: {
-    tableData: {type: Array},
-    tableTitle: {type: Array},
-    tableLoading: {type: Boolean, default: false},
-    selection: {type: Boolean, default: true},
-    index: {type: Boolean, default: false},
-    height: {type: Number || String},
-    disabled: {type: Boolean, default: false},
+    tableData: { type: Array },
+    tableTitle: { type: Array },
+    tableLoading: { type: Boolean, default: false },
+    selection: { type: Boolean, default: true },
+    index: { type: Boolean, default: false },
+    height: { type: Number || String },
+    disabled: { type: Boolean, default: false },
   },
   components: {
     iInput,
@@ -160,10 +211,10 @@ export default {
     iButton
   },
   methods: {
-    handleSelectionChange(val) {
+    handleSelectionChange (val) {
       this.$emit('handleSelectionChange', val)
     },
-    handleSelectChange(type, val, time) {
+    handleSelectChange (type, val, time) {
       const res = {
         type,
         val,
@@ -171,26 +222,26 @@ export default {
       }
       this.$emit('handleSelectChange', res)
     },
-    openPage(params) {
+    openPage (params) {
       this.$emit('openPage', params)
     },
-    handleExampleDownload(row) {
+    handleExampleDownload (row) {
       this.$emit('handleExampleDownload', row)
     },
-    handleViewAttachment(row) {
+    handleViewAttachment (row) {
       this.$emit('handleViewAttachment', row)
     },
-    viewPublish(row){
+    viewPublish (row) {
       this.$emit('viewPublish', row)
     },
-    publish(row){
+    publish (row) {
       this.$emit('publish', row)
     },
-    handleUploadedCallback(event, row) {
+    handleUploadedCallback (event, row) {
       this.$emit('handleUploadedCallback', event, row)
     },
-    handleFileDownload(row) {
-      if (row.fileName && !row.isCommitment) {
+    handleFileDownload (row) {
+      if (row.fileId && !row.isCommitment) {
         this.$emit('handleFileDownload', row)
       } else {
         return false
@@ -203,8 +254,8 @@ export default {
 .openLinkText {
   color: $color-blue;
 }
-::v-deep .text-500{
-  span{
+::v-deep .text-500 {
+  span {
     font-weight: 500 !important;
   }
 }
