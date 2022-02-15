3d262e09966e38ba1782332cca401dbe0654bb0b
zhaohuaxing <vinzhao@deloitte.com.cn>
2022-02-15 15:13:59 +0800

    深评

--- src/views/bpm/frmRatingApproval/depthRatingApproval/data.js
+++ src/views/bpm/frmRatingApproval/depthRatingApproval/data.js
@@ -1,12 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2022-02-07 10:34:27
+ * @LastEditTime: 2022-02-15 14:53:22
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \front-portal\src\views\bpm\frmRatingApproval\depthRatingApproval\data.js
+ */
 export const depthRatingApprovalTitle = [
-  {props: 'nameZh', name: '供应商名称/集团', key: 'SPR_FRM_DEP_SUPNAME'},
-  {props: 'sapCode', name: 'SAP号', key: 'SUPPLIER_SAPHAO'},
-  {props: 'previousTto', name: '上年TO', key: 'SPR_FRM_FRMGL_SNTO'},
-  {props: 'relevantDept', name: '相关科室', key: 'SPR_FRM_FRMGL_XGKS'},
-  {props: 'existShareName', name: '相关股', key: 'SPR_FRM_FRMGL_XGG'},
-  {props: 'deepCommentReasons', name: '深评原因', key: 'SPR_FRM_DEP_DEPREASON'},
-  {props: 'deepCommentOtherReasons', name: '详细原因', key: 'SPR_FRM_DEP_XXYY'},
-  {props: 'isDeepComment', name: '是否深评', key: 'SPR_FRM_DEP_ISDEPTH'},
-  {props: 'noDeepCommentReason', name: '不评级原因', key: 'SPR_FRM_DEP_BPJYY'},
+  // {props: 'nameZh', name: '供应商名称/集团', key: 'SPR_FRM_DEP_SUPNAME'},
+  // {props: 'sapCode', name: 'SAP号', key: 'SUPPLIER_SAPHAO'},
+  // {props: 'previousTto', name: '上年TO', key: 'SPR_FRM_FRMGL_SNTO'},
+  // {props: 'relevantDept', name: '相关科室', key: 'SPR_FRM_FRMGL_XGKS'},
+  // {props: 'existShareName', name: '相关股', key: 'SPR_FRM_FRMGL_XGG'},
+  // {props: 'deepCommentReasons', name: '深评原因', key: 'SPR_FRM_DEP_DEPREASON'},
+  // {props: 'deepCommentOtherReasons', name: '详细原因', key: 'SPR_FRM_DEP_XXYY'},
+  // {props: 'isDeepComment', name: '是否深评', key: 'SPR_FRM_DEP_ISDEPTH'},
+  // {props: 'noDeepCommentReason', name: '不评级原因', key: 'SPR_FRM_DEP_BPJYY'},
+  { props: 'sapCode', name: 'SAP号', key: 'SUPPLIER_SAPHAO' },
+  { props: 'previousTto', name: '上年TO', key: 'SPR_FRM_FRMGL_SNTO' },
+  { props: 'relevantDept', name: '相关科室', key: 'SPR_FRM_FRMGL_XGKS' },
+  { props: 'existShareName', name: '相关股', key: 'SPR_FRM_FRMGL_XGG' }
 ]
-
