8bda0777393e943559821b370bbb4126b42b88cd
ATRAPA\jerryqwang <jerryqwang@deloitte.com.cn>
2022-03-01 10:06:50 +0800

    消息管理

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -1,6 +1,8 @@
 import online from './online'
 import mainData from './mainData'
+import messageManagement from './messageManagement'
 export default {
   ...online,
-  ...mainData
+  ...mainData,
+  ...messageManagement
 }
