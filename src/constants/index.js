5ad4d61291aa4d6b01113a06ef7c5ab35ffe1068
ArthurLyu <artlyu@deloitte.com.cn>
2022-03-14 14:42:41 +0800

    测试EKL参数

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,15 +1,15 @@
 export const MAP_APPROVAL_TYPE = {
-  AGREE: 1,
-  REFUSE: 2,
-  APPEND_DATA: 3
+    AGREE: 1,
+    REFUSE: 2,
+    APPEND_DATA: 3
 }
 
 export const MOCK_FILE_URL = '/udDown'
-// export const MOCK_FILE_URL = '/getFileByFileId'
+    // export const MOCK_FILE_URL = '/getFileByFileId'
 
 //EventBus 名字
 export const EventBusName = {
-  SelectedOrganizationTag: 'SelectedOrganizationTag'
+    SelectedOrganizationTag: 'SelectedOrganizationTag'
 }
 
 // aeko审批类型
@@ -17,73 +17,72 @@ export const AEKO_CATEGORY_LIST = ['aeko_mp', 'aeko_aea', 'aeko_content']
 
 // 审批列表只能单选的类型
 export const BPM_SINGL_CATEGORY_LIST = [
-  'meeting_rs_recheck',
-  'meeting_recheck_m_sign',
-  'officialPricing',
-  'mtz_node_approval'
-]
-// 'officialPricing'
-// 审批有异议、无异议判断
+        'meeting_rs_recheck',
+        'meeting_recheck_m_sign',
+        'officialPricing',
+        'mtz_node_approval'
+    ]
+    // 'officialPricing'
+    // 审批有异议、无异议判断
 export const BPM_CATEGORY_RENAME_YIYI_LIST = [
-  'meeting_rs_recheck',
-  'mtz_node_approval'
+    'meeting_rs_recheck',
+    'mtz_node_approval'
 ]
 
 // 审批单据类型
 /**
  * 单据类型
  */
-export const BPM_APPROVAL_TYPE_OPTIONS = [
-  {
-    value: '-1',
-    label: '未知类型'
-  },
-  {
-    value: '0',
-    label: '供应商管理'
-  },
-  {
-    value: '1',
-    label: '供应商转正'
-  },
-  {
-    value: '2',
-    label: '供应商修改'
-  },
-  {
-    value: '4',
-    label: '供应商准入'
-  },
-  {
-    value: '5',
-    label: '价格管理'
-  },
-  {
-    value: '6',
-    label: '寻源执行'
-  },
-  {
-    value: '7',
-    label: 'ProCS'
-  },
-  {
-    value: '8',
-    label: '寻源与定点'
-  },
-  {
-    value: '9',
-    label: '订单与合同'
-  },
-  {
-    value: '10',
-    label: 'AEKO'
-  },
-  {
-    value: '11',
-    label: '模具采购'
-  },
-  {
-    value: '12',
-    label: 'MTZ'
-  }
+export const BPM_APPROVAL_TYPE_OPTIONS = [{
+        value: '-1',
+        label: '未知类型'
+    },
+    {
+        value: '0',
+        label: '供应商管理'
+    },
+    {
+        value: '1',
+        label: '供应商转正'
+    },
+    {
+        value: '2',
+        label: '供应商修改'
+    },
+    {
+        value: '4',
+        label: '供应商准入'
+    },
+    {
+        value: '5',
+        label: '价格管理'
+    },
+    {
+        value: '6',
+        label: '寻源执行'
+    },
+    {
+        value: '7',
+        label: 'ProCS'
+    },
+    {
+        value: '8',
+        label: '寻源与定点'
+    },
+    {
+        value: '9',
+        label: '订单与合同'
+    },
+    {
+        value: '10',
+        label: 'AEKO'
+    },
+    {
+        value: '11',
+        label: '模具采购'
+    },
+    {
+        value: '12',
+        label: 'MTZ'
+    }
 ]
--- src/views/purchase/achievement/mymerit/components/pfjwfbmsj.vue
+++ src/views/purchase/achievement/mymerit/components/pfjwfbmsj.vue
@@ -13,7 +13,7 @@
           cleintId: '66e3fa4b-152e-4bfb-8a9d-18b820459df1',
           tenantId: '8716a3b9-b827-484b-92ad-ed90aaf4d707',
           clientSecret: '6cpyeTY~dAmL-Uub2X8Om30Mm2EUk-d.TS',
-          workspaceId: process.env.NODE_ENV !== 'UAT' ? 'addf20a8-7f65-445c-a737-0f636f428e76' : '876776a9-f959-442e-a011-b4bade0dd862',
+          workspaceId: process.env.EKL_BAOBIAO_PARAMS.pfjwfbmsj.workspaceId
           reportId: process.env.NODE_ENV !== 'UAT' ? 'c272ae69-a6b4-4407-bd0e-f67953de36ce' : 'f8d1c804-6583-4574-ba06-1f591aa58cd4',
           datasets: process.env.NODE_ENV !== 'UAT' ? ['0571b1ad-b958-4057-aff9-7b9239c811b6'] : ['addf20a8-7f65-445c-a737-0f636f428e76'],
           username: this.$store.state.permission.userInfo.id,
@@ -22,6 +22,8 @@
       }
     },
     mounted() {
+      console.log(process.env, this.workspaceId);
+      
       this.$emit('getData', this.formData)
     },
 
