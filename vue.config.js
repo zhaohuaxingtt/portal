3f1b453018c051d627def0ea3af1ffdc50dd753c
qyu <ext-yuqiang@csvw.com>
2022-03-14 17:19:29 +0800

    mainData 国际化

--- vue.config.js
+++ vue.config.js
@@ -13,8 +13,8 @@ const postcss = px2rem({
 })
 
 // const BASE_IP = 'http://rise-nginx-internal.apps.vmocp-uat.csvw.com'
-// const BASE_IP = '10.122.18.166'
-const BASE_IP = '10.122.17.38'
+const BASE_IP = '10.122.18.166'
+// const BASE_IP = '10.122.17.38'
 
 module.exports = {
   publicPath: process.env.VUE_APP_PUBLICPATH,
@@ -162,17 +162,17 @@ module.exports = {
         }
       },
       [process.env.VUE_APP_USER_CENTER]: {
-        // target: `http://${BASE_IP}:8015/usercenter/`,
-        target:
-          'http://rise-gateway-runtime.apps.vmocp-uat.csvw.com/usercenter/',
+        target: `http://${BASE_IP}:8015/usercenter/`,
+        // target:
+        //   'http://rise-gateway-runtime.apps.vmocp-uat.csvw.com/usercenter/',
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_USER_CENTER]: ''
         }
       },
       [process.env.VUE_APP_APPROVAL]: {
-        // target: `http://${BASE_IP}:8012/approval`,
-        target: `http://rise-nginx-internal.apps.vmocp-uat.csvw.com/approvalApi`,
+        target: `http://${BASE_IP}:8012/approval`,
+        // target: `http://rise-nginx-internal.apps.vmocp-uat.csvw.com/approvalApi`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_APPROVAL]: ''
