7a2c549f503869e56436f876238d58003e02c515
tanmou <1198593817@qq.com>
2022-03-18 09:30:03 +0800

    hotfix-CRW-6962 已修正frm接口

--- src/views/generalPage/frm/components/data.js
+++ src/views/generalPage/frm/components/data.js
@@ -1,26 +1,14 @@
 export const frmTitle=[
-	[
-		{key:'a',name:'新供应商评级结果',language: 'XINGONGYINGSHANGPINGJIJIEGUO', permission: 'SUPPLIER_FRM_NEWSUPPLIERRATINGRESULT'},
-		{key:'b',name:'新供应商评级时间',language: 'XINGONGYINGSHANGPINGJISHIJIAN', permission: 'SUPPLIER_FRM_NEWSUPPLIERRATINGTIME'}
-	],
-	
-	[
-		{key:'c',name:'外部评级',language: 'WAIBUPINGJI', permission: 'SUPPLIER_FRM_EXTERNALRATING'},
-		{key:'d',name:'外部评价',language: 'SUPPLIER_WAIBUPINGJIA', permission: 'SUPPLIER_FRM_EXTEREVALUATE'},
-		{key:'e',name:'外部评级时间',language: 'WAIBUPINGJIESHIJIAN', permission: 'SUPPLIER_FRM_EXTERNALRATINGTIME'}
-	],
-	
-	[
-		{key:'f',name:'初评调整后得分',language: 'CHUBUTIAOZHENGHOUDEFEN', permission: 'SUPPLIER_FRM_ADJUSTEDSCOREAFTERINITIALRATING'},
-		{key:'g',name:'初评调整后结果',language: 'CHUPINGTIAOZHENGHOUJIEGUO', permission: 'SUPPLIER_FRM_ADJUSTEDRESULTAFTERINITIALRATING'},
-		{key:'h',name:'初评时间',language: 'CHUPINGSHIJIAN', permission: 'SUPPLIER_FRM_INITIALRATINGTIME'}
-	],
-	[
-		{key:'i',name:'深入评级结果',language: 'SHENGRUPINGJIJIEGUO', permission: 'SUPPLIER_FRM_INDEPTHANALYSISRESULT'},
-		{key:'j',name:'深入评级时间',language: 'SHENGRUPINGJISHIJIAN', permission: 'SUPPLIER_FRM_INDEPTHANALYSISTIME'}
-	],
-	[
-		{key:'k',name:'VWAG评级结果',language: 'VWAGPINGJIJIEGUO', permission: 'SUPPLIER_FRM_VMAG_RATINGRESULT'},
-		{key:'l',name:'VWAG评级时间',language: 'VWAGPINGJISHIJIAN', permission: 'SUPPLIER_FRM_VMAG_RATINGTIME'}
-	]
+	{key:'ratingResultText',name:'新供应商评级结果',language: 'XINGONGYINGSHANGPINGJIJIEGUO', permission: 'SUPPLIER_FRM_NEWSUPPLIERRATINGRESULT'},
+	{key:'markingTime',name:'新供应商评级时间',language: 'XINGONGYINGSHANGPINGJISHIJIAN', permission: 'SUPPLIER_FRM_NEWSUPPLIERRATINGTIME'},
+	{key:'externalEvaluationLevel',name:'外部评级',language: 'WAIBUPINGJI', permission: 'SUPPLIER_FRM_EXTERNALRATING'},
+	{key:'externalEvaluation',name:'外部评价',language: 'SUPPLIER_WAIBUPINGJIA', permission: 'SUPPLIER_FRM_EXTEREVALUATE'},
+	{key:'externalEvaluationLevelDate',name:'外部评级时间',language: 'WAIBUPINGJIESHIJIAN', permission: 'SUPPLIER_FRM_EXTERNALRATINGTIME'},
+	{key:'afterAdjustScore',name:'初评调整后得分',language: 'CHUBUTIAOZHENGHOUDEFEN', permission: 'SUPPLIER_FRM_ADJUSTEDSCOREAFTERINITIALRATING'},
+	{key:'afterGrade',name:'初评调整后结果',language: 'CHUPINGTIAOZHENGHOUJIEGUO', permission: 'SUPPLIER_FRM_ADJUSTEDRESULTAFTERINITIALRATING'},
+	{key:'qualitativeScoreDate',name:'初评时间',language: 'CHUPINGSHIJIAN', permission: 'SUPPLIER_FRM_INITIALRATINGTIME'},
+	{key:'deepCommentResult',name:'深入评级结果',language: 'SHENGRUPINGJIJIEGUO', permission: 'SUPPLIER_FRM_INDEPTHANALYSISRESULT'},
+	{key:'approvalEndDate',name:'深入评级时间',language: 'SHENGRUPINGJISHIJIAN', permission: 'SUPPLIER_FRM_INDEPTHANALYSISTIME'},
+	{key:'vwagAssessResult',name:'VWAG评级结果',language: 'VWAGPINGJIJIEGUO', permission: 'SUPPLIER_FRM_VMAG_RATINGRESULT'},
+	{key:'vwagDate',name:'VWAG评级时间',language: 'VWAGPINGJISHIJIAN', permission: 'SUPPLIER_FRM_VMAG_RATINGTIME'},
 ]
