fac9c4af7228e3abe6531141018ddf783c67da20
qyu <ext-yuqiang@csvw.com>
2022-05-06 11:28:13 +0800

    CRW-7366【Meeting】CF会议中心里的会议议题展示界面

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ const postcss = px2rem({
 // const BASE_IP = 'http://rise-nginx-internal.apps.vmocp-dev.csvw.com'
 // const BASE_IP = '10.122.18.166'
 // const BASE_IP = '10.122.17.38'
-const BASE_IP = "http://rise-gateway-runtime.apps.vmocp-dev.csvw.com";
+const BASE_IP = 'http://rise-gateway-runtime.apps.vmocp-uat.csvw.com'
 
 console.log(BASE_IP)
 
@@ -204,9 +204,9 @@ module.exports = {
       },
       // ------------------ 上传 ----------------------------
       '/fileApi': {
-        target: `${BASE_IP}/`,
+        // target: `${BASE_IP}`,
         // target: `http://${BASE_IP}:8034`,
-        // target: `http://rise-nginx-internal.apps.vmocp-test.csvw.com/fileApi`,
+        target: `http://rise-nginx-internal.apps.vmocp-uat.csvw.com/fileApi`,
         changeOrigin: true,
         pathRewrite: {
           '/fileApi': ''
