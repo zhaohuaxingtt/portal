4148f7ec346639df8cf4eddb21c5c4704ca2b950
余继鹏 <917955345@qq.com>
2022-09-21 18:07:50 +0800

    update

--- src/views/capacityManagement/capacityMaintenanceTask/index.vue
+++ src/views/capacityManagement/capacityMaintenanceTask/index.vue
@@ -72,10 +72,11 @@
             <i-button @click="exportTaskList">{{
               language('批量导出', '批量导出')
             }}</i-button>
-            <icon name="iconSetting" class="margin-left10" symbol />
+            <buttonTableSetting @click="edittableHeader" />
           </template>
           <template slot="body">
             <table-list
+              ref="tableList"
               height="100%"
               :selectConfig="{ width: 42 }"
               :tableData="tableDataRight"
@@ -162,6 +163,8 @@ import {
 } from './data'
 import { pageMixins } from '@/utils/pageMixins'
 import tableList from 'rise/web/components/iTableSort'
+import buttonTableSetting from '@/components/buttonTableSetting'
+import { tableSortMixins } from 'rise/web/components/iTableSort/tableSortMixins'
 import {
   getUnfinishTaskList,
   getUnfinishTaskListBySupplier,
@@ -181,9 +184,10 @@ export default {
     iButton,
     icon,
     tableList,
-    iPagination
+    iPagination,
+    buttonTableSetting
   },
-  mixins: [pageMixins],
+  mixins: [pageMixins, tableSortMixins],
   data() {
     return {
       searchForm,
