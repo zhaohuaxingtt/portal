94e90ff6e011dd5999c67eb67183a5be79bd9b9e
DESKTOP-MMK98F5\Administrator <CS>
2023-02-15 13:36:52 +0800

    add

--- src/views/supplierManagement/supplierKPI/module/Completed.vue
+++ src/views/supplierManagement/supplierKPI/module/Completed.vue
@@ -1,29 +1,46 @@
 <template>
-  <div class="page">
-    <img :src="pc" >
-    <p>您已成功提交打分任务！</p>
-    <span>温馨提示：</span>
-    <span>
-      于2023.01.2323:59:59之前您都可以对打分结果进行修改、重新提交。截止时间过后则不可再进行修改。
-    </span>
+  <div>
+    <div style="text-align:right">
+    <iButton style="margin-left: 10px" @click="back">上一步</iButton>
+
+      </div>
+    <div class="page">
+   
+      <img :src="pc">
+      <p>您已成功提交打分任务！</p>
+      <span>温馨提示：</span>
+      <span>
+        于{{ infoData.endDate }}之前您都可以对打分结果进行修改、重新提交。截止时间过后则不可再进行修改。
+      </span>
+    </div>
   </div>
+
 </template>
 
 <script>
 import pc from '@/assets/images/Completed.png'
 import { iCard } from 'rise'
+import {
+
+  iButton,
+} from 'rise'
 export default {
   components: {
-    iCard
+    iButton
   },
   props: {
-    col: {}
+    infoData: { type: Object }
   },
-  data(){
+  data() {
     return {
-        pc
+      pc
     }
-  }
+  },
+  methods: {
+    back() {
+      this.$emit('back')
+    }
+  },
 }
 </script>
 <style lang="scss" scoped>
@@ -32,18 +49,21 @@ export default {
   width: 40%;
   margin-left: 30%;
   text-align: center;
+
   p {
     font-size: 18px;
     font-weight: 600;
     margin: 20px 0;
   }
+
   span {
     text-align: left;
     margin: 10px 0;
     font-size: 16px;
     display: block;
   }
-  img{
+
+  img {
     width: 200px;
     display: inline-block;
     height: 200px;
