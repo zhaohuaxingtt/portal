81f0822719cfb5b6445348fc6a3e796c603a170e
LAPTOP-QTHJQS3E\forez <1076613617@qq.com>
2023-02-17 10:40:33 +0800

    add 修改mtz顶点详情修改状态

--- src/views/mtz/annualGeneralBudget/locationChange/components/MtzLocationPoint/components/applyInfor/theDataTabs.vue
+++ src/views/mtz/annualGeneralBudget/locationChange/components/MtzLocationPoint/components/applyInfor/theDataTabs.vue
@@ -1013,7 +1013,7 @@ export default {
       return this.$store.state.location.mtzObject
     },
     isEditNew: function () {
-      return (this.appStatus == '草稿' || this.appStatus == '未通过')||(((this.flowType=='SIGN'||this.flowType=='FILING')||(['02','03',null,'01'].includes(this.meetingStatus)&&this.flowType=='MEETING'))&&this.appStatus=='已提交')
+      return (this.appStatus == '草稿' || this.appStatus == '未通过')||(((this.flowType=='SIGN'||this.flowType=='FILING')&&this.appStatus=='已提交')||(this.appStatus!='冻结'&&this.flowType=="MEETING"))
     }
   },
   watch: {
