ada8545f1ef6fb2b405218bd5017dfafa35d33d7
qyan <qyan@deloitte.com.cn>
2023-02-21 15:32:33 +0800

    [FIX]【首页】iAgree的排序

--- src/views/home/components/Approve/index.vue
+++ src/views/home/components/Approve/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="task-container">
-    <template v-for="item in moduleData">
-      <div :key="item.value" v-if="(!showPendingApproval && getLaunchNum(item.subType)) || (showPendingApproval && getTodoNum(item.subType))" class="task-card">
+  <div class="task-container" v-loading="loadingApproveData">
+    <template v-for="item in curModuleData">
+      <div :key="item.value" class="task-card">
         <div class="top">
           <div class="name single-no-ellipsis">{{ item.value }}</div>
   <!--        <div class="abs single-ellipsis">{{ getAbs(item.typeName) }}</div>-->
@@ -58,7 +58,14 @@ export default {
     // eslint-disable-next-line no-undef
     ...Vuex.mapState({
       userInfo: state => state.permission.userInfo
-    })
+    }),
+    curModuleData: function() {
+      if(this.showPendingApproval) {
+        return this.todoModuleData
+      } else {
+        return this.launchModuleData
+      }
+    }
   },
   filters: {
     overNum: function(value) {
@@ -69,6 +76,8 @@ export default {
     return {
       valueNumbers: {},
       moduleData: [],
+      todoModuleData: [], //待办的data，已经排序
+      launchModuleData: [], //我的申请的data，已经排序
       absMap: {},
       dataList:[],
       dataListNow:[],
@@ -77,7 +86,8 @@ export default {
         name:"",
         applyNumber:"",//申请
         approvalNumber:"",//审批
-      }
+      },
+      loadingApproveData: false
     }
   },
   watch: {
@@ -86,9 +96,9 @@ export default {
     //   console.log("moduleData, approvalToDoNum watch...", this.moduleData)
     // }
   },
-  created() {
-    this.queryAllData()
-    this.initModuleData()
+  async created() {
+    await this.queryAllData()
+    // this.initModuleData()
   },
   mounted(){
     // this.$nextTick(()=>{
@@ -175,23 +185,38 @@ export default {
       // console.log('data=>',data);
       // if (filteredModuleData.length <= 5) {
       this.moduleData = filteredModuleData
+      if(this.moduleData && this.moduleData.length > 0) {
+        this.todoModuleData = this.moduleData.filter(e => e.todoNum).sort((a, b) => {
+          return b.todoNum - a.todoNum
+        })
+        this.launchModuleData = this.moduleData.filter(e => e.launchNum).sort((a, b) => {
+          return b.launchNum - a.launchNum
+        })
+      }
         // console.log("moduleData, approvalToDoNum...", this.moduleData, approvalToDoNum)
       // } else {
       //   this.moduleData = filteredModuleData.slice(0, 5)
       // }
     },
     async queryAllData() {
-      const result = await getApprovalList({ userID: this.userInfo.id })
-      // console.log('queryAllData getApprovalList result=>',result);
-      const data = result?.data || []
-      data.forEach(item => {
-        this.absMap[item.typeName] = item.typeValue
-        item.wfCategoryList.forEach(e => {
-          this.valueNumbers[e.subType] = { ...e }
+      this.loadingApproveData = true
+      try {
+        const result = await getApprovalList({ userID: this.userInfo.id })
+        // console.log('queryAllData getApprovalList result=>',result);
+        const data = result?.data || []
+        data.forEach(item => {
+          this.absMap[item.typeName] = item.typeValue
+          item.wfCategoryList.forEach(e => {
+            this.valueNumbers[e.subType] = { ...e }
+          })
         })
-      })
-      this.data = data
-      this.initModuleData()
+        this.data = data
+        this.initModuleData()
+      } catch (error) {
+        console.log(error)
+      } finally {
+        this.loadingApproveData = false
+      }
     },
     getLaunchNum(subType) {
       if (this.valueNumbers[subType]) {
@@ -306,6 +331,7 @@ export default {
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
+  height: 100%;
   .task-card {
     width: calc(50% - 5px);
     height: 120px;
