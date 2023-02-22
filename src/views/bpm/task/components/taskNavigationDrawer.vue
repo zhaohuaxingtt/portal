ffa0fa1e3b80c10bf7c5507e7a56077673314b18
qyan <qyan@deloitte.com.cn>
2023-02-22 09:43:30 +0800

    [FIX]【审批详情】上下循环着显示

--- src/views/bpm/task/components/taskNavigationDrawer.vue
+++ src/views/bpm/task/components/taskNavigationDrawer.vue
@@ -83,28 +83,7 @@
     methods: {
       handleFinishCurrentTask(instanceId) {
         if(this.todoTaskList?.length >= 2) {
-          let curIndex = -1
-          this.todoTaskList.find((item,index) => {
-            if(item.instanceId === instanceId) {
-              curIndex = index
-            }
-          })
-          // 找到当前finish了的Index
-          if((curIndex+1) < this.todoTaskList.length) {
-            this.$nextTick(() => {
-              this.gotoDetailPage(this.todoTaskList[curIndex + 1])
-            })
-            return true
-          } else if((curIndex - 1) >= 0){
-            this.$nextTick(() => {
-              this.gotoDetailPage(this.todoTaskList[curIndex - 1])
-            })
-            return true
-          } else {
-            this.$message.info(`已经是最后一条`)
-            this.$emit("showMoreTaskNeedClose")
-            return false
-          }
+          return this.gotoNextTask(instanceId)
         } else {
           this.$message.info(`已经是最后一条`)
           return false
@@ -120,31 +99,31 @@
           })
           // 找到当前finish了的Index
           if(goPreTask) {
-            if((curIndex - 1) < this.todoTaskList.length) {
+            if((curIndex - 1) < 0 ) {
               this.$nextTick(() => {
-                this.gotoDetailPage(this.todoTaskList[curIndex - 1])
+                this.gotoDetailPage(this.todoTaskList[this.todoTaskList.length - 1])
               })
               return true
-            } else if((curIndex + 1) >= 0){
+            } else {
               this.$nextTick(() => {
-                this.gotoDetailPage(this.todoTaskList[curIndex + 1])
+                this.gotoDetailPage(this.todoTaskList[curIndex - 1])
               })
               return true
             }
           } else {
-            if((curIndex + 1) < this.todoTaskList.length) {
+            if((curIndex + 1) >= this.todoTaskList.length) {
+              this.$nextTick(() => {
+                this.gotoDetailPage(this.todoTaskList[0])
+              })
+              return true
+            } else {
               this.$nextTick(() => {
                 this.gotoDetailPage(this.todoTaskList[curIndex + 1])
               })
               return true
-            } else if((curIndex - 1) >= 0){
-              this.$nextTick(() => {
-                this.gotoDetailPage(this.todoTaskList[curIndex - 1])
-              })
-              return true
             }
           }
-          
+
           this.$message.info(`已经是最后一条`)
           return false
         } else {
@@ -183,9 +162,13 @@
             this.$nextTick(() => {
               this.gotoDetailPage(this.todoTaskList[curIndex])
             })
+            return true
           } else {
-            this.handleLoopTask(instanceId, false)
+            return this.handleLoopTask(instanceId, false)
           }
+        } else {
+          this.$message.info(`已经是最后一条`)
+          return false
         }
       },
       getQueryTodoListFunc() {
