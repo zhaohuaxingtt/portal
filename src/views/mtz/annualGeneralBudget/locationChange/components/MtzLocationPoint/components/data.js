d3d9dc915b980cc1c9b765e3fddaa0e8eb4d4cb6
余继鹏 <917955345@qq.com>
2023-03-07 19:26:20 +0800

    UI优化

--- src/views/mtz/annualGeneralBudget/locationChange/components/MtzLocationPoint/components/data.js
+++ src/views/mtz/annualGeneralBudget/locationChange/components/MtzLocationPoint/components/data.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-02-07 10:34:32
- * @LastEditTime: 2022-03-02 17:01:29
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2023-03-07 18:53:57
+ * @LastEditors: YoHo && 917955345@qq.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \front-portal\src\views\mtz\annualGeneralBudget\locationChange\components\MtzLocationPoint\components\data.js
  */
@@ -50,6 +50,86 @@ export const tableTitle = [
   { props: 'linieDeptName', name: '科室', key: 'KESHI' }
   // { props: 'isRsFreezed', name: 'RS单状态', key: 'RSDANZHUANGTAI' },
   // { props: 'rsFreezeDate', name: 'RS冻结时间', key: 'RSDONGJIESHIJIAN' },
+]
+export const tableTitle1 = [
+  {
+    type: 'selection',
+    width: 50,
+  }, {
+    type: 'index',
+    label:'#',
+    width: 50,
+  },
+  {
+    prop: 'id', name: '申请单号', i18n: 'SHENQINGDANHAO', width: 120, emit: 'handleClickFsupplierName', customRender: (h, scope, c, extraData) => {
+      if (scope.row.viewDetailsFlag) {
+        return <span class="link">{scope.row.id}</span>
+      }
+      return <span>{scope.row.id}</span>
+    }
+  },
+  {
+    prop: 'ttNominateAppId', name: '关联申请单号', i18n: 'GLSQDH', width: 160, emit: 'handleClickTtNominateAppId', customRender: (h, scope, c, extraData) => {
+      return <span class="link">{scope.row.ttNominateAppId}</span>
+    }
+  },
+  {
+    prop: 'appName',
+    name: '申请单名称',
+    i18n: 'SHENQINGDANMINGCHENG',
+    width: 300,
+    tooltip: true
+  },
+  {
+    prop: 'flowTypeDesc',
+    name: '流程类型',
+    i18n: 'LIUCHENGLEIXING',
+    width: 100
+  },
+  {
+    prop: 'appStatusDesc',
+    name: '申请状态',
+    i18n: 'SHENQINGZHUANGTAI',
+    width: 100
+  },
+  {
+    prop: 'meetingName',
+    name: '会议',
+    i18n: 'HUIYI',
+    width: 300
+  },
+  {
+    prop: 'freezeDate',
+    name: '冻结时间',
+    i18n: 'DONGJIESHIJIAN',
+    width: 125,
+    customRender: (h, scope, c, extraData) => {
+      return <div><p class="date-time-cell">
+        {scope.row.freezeDate ? scope.row.freezeDate.split(' ')[0] : ''}
+      </p>
+        <p class="date-time-cell">
+          {scope.row.freezeDate ? scope.row.freezeDate.split(' ')[1] : ''}
+        </p></div>
+    }
+  },
+  {
+    prop: 'nominateDate',
+    name: '定点时间',
+    i18n: 'DINGDIANSHIJIAN',
+    width: 125,
+    customRender: (h, scope, c, extraData) => {
+      return <div><p class="date-time-cell">
+        {scope.row.nominateDate ? scope.row.nominateDate.split(' ')[0] : ''}
+      </p>
+        <p class="date-time-cell">
+          {scope.row.nominateDate ? scope.row.nominateDate.split(' ')[1] : ''}
+        </p></div>
+    }
+  },
+  { prop: 'buyer', name: '采购员', i18n: 'CAIGOUYUAN' },
+  { prop: 'linieDeptName', name: '科室', i18n: 'KESHI' }
+  // { prop: 'isRsFreezed', name: 'RS单状态', i18n: 'RSDANZHUANGTAI' },
+  // { prop: 'rsFreezeDate', name: 'RS冻结时间', i18n: 'RSDONGJIESHIJIAN' },
 ]
 
 export const tableTitleCp = [
