9ec578d9b463016f373529baf9758fcbc71b67c7
余继鹏 <917955345@qq.com>
2023-03-23 15:30:51 +0800

    update

--- src/api/file/index.js
+++ src/api/file/index.js
@@ -1,17 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2021-11-09 15:26:18
- * @LastEditTime: 2023-03-06 15:38:07
+ * @LastEditTime: 2023-03-22 15:33:24
  * @LastEditors: YoHo && 917955345@qq.com
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \front-portal\src\api\file\index.js
  */
 import axios from '@/utils/axios.download'
+import httpAxios from '@/utils/axios'
 import { serialize } from '@/utils'
 
 const requst = axios(process.env.VUE_APP_COMMON)
 const fileRequst = axios(process.env.VUE_APP_FILEAPI + '/fileud')
-const wopi = axios(process.env.VUE_APP_FILEAPI)
+const wopi = httpAxios(process.env.VUE_APP_FILEAPI)
 
 export function downloadFile(parmars) {
   return requst({
