c98b0e726e5a0019508efce1e63f95afc3d287ac
LAPTOP-QTHJQS3E\forez <1076613617@qq.com>
2023-03-30 18:19:47 +0800

    fix 添加版本

--- src/views/mtz/dataBase/rulesQuery/components/data.js
+++ src/views/mtz/dataBase/rulesQuery/components/data.js
@@ -40,7 +40,42 @@ const renderIcon = (h, column, type) => {
     )
   ])
 }
-
+export const avgPeriodList = [
+  { code: 1, name: '一月',  },
+  { code: 2, name: '二月',  },
+  { code: 3, name: '三月',  },
+  { code: 4, name: '四月',  },
+  { code: 6, name: '六月',  },
+  { code: 12, name: '十二月',  },
+  { code: 0, name: '补差时间段内均值',  },
+]
+export const offsetList = [
+  { code: 1, name: '1个月',  },
+  { code: 2, name: '2个月',  },
+  { code: 3, name: '3个月',  },
+  { code: 4, name: '4个月',  },
+  { code: 5, name: '5个月',  },
+  { code: 6, name: '6个月',  },
+  { code: 7, name: '7个月',  },
+  { code: 8, name: '8个月',  },
+  { code: 9, name: '9个月',  },
+  { code: 10, name: '10个月',  },
+  { code: 11, name: '11个月',  },
+  { code: 12, name: '12个月',  },
+  { code: 0, name: '0个月',  },
+  { code: -1, name: '-1个月',  },
+  { code: -2, name: '-2个月',  },
+  { code: -3, name: '-3个月',  },
+  { code: -4, name: '-4个月',  },
+  { code: -5, name: '-5个月',  },
+  { code: -6, name: '-6个月',  },
+  { code:-7, name: '-7个月',  },
+  { code: -8, name: '-8个月',  },
+  { code: -9, name: '-9个月',  },
+  { code: -10, name: '-10个月',  },
+  { code: -11, name: '-11个月',  },
+  { code: -12, name: '-12个月',  },
+]
 // 员工列表Table配置
 export const tableSetting = [
   { type: 'selection', width: 50 },
@@ -88,7 +123,10 @@ export const tableSetting = [
     align: 'center',
     label: '计算偏移(月)',
     i18n: '计算偏移(月)',
-    width: 120
+    width: 120,
+    customRender: (h, scope) => {
+      return   <span>{ scope.row.offsetMonth||scope.row.offsetMonth=='0' ? offsetList.find(val => val.code == scope.row.offsetMonth).name : '' }</span>
+    }
   },
   {
     prop: 'avgPeriod',
@@ -96,9 +134,13 @@ export const tableSetting = [
     align: 'center',
     label: '均值计算周期(月)',
     i18n: '均值计算周期(月)',
-    width: 140
+    width: 140,
+    customRender: (h, scope) => {
+      return   <span> {scope.row.avgPeriod||scope.row.avgPeriod=='0' ? avgPeriodList.find(val => val.code == scope.row.avgPeriod).name : ''} </span>
+    }
   },
   { prop: 'price', label: '基价', align: 'center', i18n: '基价', width: 150 },
+
   {
     prop: 'priceMeasureUnit',
     label: '基价计量单位',
@@ -141,6 +183,13 @@ export const tableSetting = [
     label: '补差周期',
     align: 'center',
     i18n: '补差周期',
+    width: 150
+  },
+  {
+    prop: 'ruleVersion',
+    label: '版本',
+    align: 'center',
+    i18n: '版本',
     width: 150
   },
   {
