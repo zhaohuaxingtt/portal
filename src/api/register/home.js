752fef6b84a6320b105e64aa6110f31340e06744
余继鹏 <917955345@qq.com>
2023-06-06 18:02:35 +0800

    update

--- src/api/register/home.js
+++ src/api/register/home.js
@@ -4,7 +4,13 @@
  * @Description: In User Settings Edit
  */
 import axios from '@/utils/axios'
+import md5 from '@/utils/md5'
 const requst = axios(process.env.VUE_APP_SUPPLIER + '/web')
+function getSigntoken(purchaserEmail){
+  return {
+    signtoken:md5(purchaserEmail + '|' + 'rise3edc$RFV')
+  }
+}
 
 // 校验是否已经注册用户
 export function indexNext(parmars, type) {
@@ -45,6 +51,7 @@ export function verification(parmars, type) {
 //通过email ，查询采购员信息
 export function getUserInfo(parmars, type) {
   return requst({
+    headers:getSigntoken(parmars.purchaserEmail),
     url: `/basic/getUserInfo`,
     method: 'POST',
     data: parmars,
